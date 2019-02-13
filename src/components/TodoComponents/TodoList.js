3056123c1d704ad1234e03cae33d474dac6f6e67
Kim Duclos <kimduclos@gmail.com>
2019-02-13 13:59:03 -0700

    todo list built

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,9 +4,14 @@ import Todo from './Todo';
 // your components will all go in this `component` directory.
 
 const TodoList = props => {
-    <div className="listComponent">
-        
-    </div>
-}
+    return (
+        <div className="listComponent">
+            {props.tasksTodo.map(taskToDo => (
+                // component to output tasks
+                <taskToDo complete={props.complete} idNum={taskToDo.id} taskToDo={taskToDo}/>
+            ))}
+        </div>
+     );
+};
 
-
+export default Todo;
